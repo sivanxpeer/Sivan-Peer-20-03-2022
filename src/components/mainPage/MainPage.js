486640ce33e190f2947a75841e71861ea07f09e9
sivanxpeer <sivan.x.peer@gmail.com>
2022-03-21 11:53:20 +0200

    boilerplate

---
+++ src/components/mainPage/MainPage.js
@@ -0,0 +1,14 @@
+import "./MainPage.css";
+
+import React from 'react'
+import SearchBar from "../searchBar/SearchBar";
+
+const MainPage = () => {
+  return (
+    <div className="main-page">
+        <SearchBar></SearchBar>
+    </div>
+  )
+}
+
+export default MainPage
---
+++ src/components/searchBar/SearchBar.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react'
+import "./SearchBar.css"
+
+const SearchBar = () => {
+    const [location, setLocation] = useState("Tel Aviv");
+    const handleInputChange = (e) => {
+        setLocation(e.target.value);
+    }
+    const handleFormSubmit = (event) => {
+        event.preventDefault();
+        setLocation(event.target.value);
+    }
+    //https://developer.accuweather.com/user/me/apps
+    //API KEY 2GJ4abggxGIzQhsQ4S7DZTyfIUkOSzqC
+
+    //autocomplete endpoint: 
+    // http://dataservice.accuweather.com/locations/v1/cities/autocomplete
+
+    // get current weather endpoint:
+    // http://dataservice.accuweather.com/currentconditions/v1/{locationKey}
+
+    // 5-daily forecast endpoint:
+    // http://dataservice.accuweather.com/forecasts/v1/daily/5day/{locationKey}
+
+
+
+
+
+    return (
+        <>
+            <form onSumbit={(e) => handleFormSubmit(e)} className="search-bar">
+                <input type="text" className="input-search" onChange={e => handleInputChange(e)} value={location}></input>
+                {/* <select>
+                    <option>Eilat</option>
+                    <option>New York</option>
+                </select> */}
+                <button type="submit" className="btn-search">Search</button>
+            </form>
+        </>
+    )
+}
+
+export default SearchBar
