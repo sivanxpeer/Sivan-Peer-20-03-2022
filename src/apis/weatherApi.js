2542a4105f8e9c0440d76902f65693d7b722b41d
sivanxpeer <sivan.x.peer@gmail.com>
2022-03-25 12:28:09 +0300

    https for deploy

--- src/apis/weatherApi.js
+++ src/apis/weatherApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // const city = "tel%20aviv";
-const baseURL = `http://dataservice.accuweather.com//forecasts/v1/`;
+const baseURL = `https://dataservice.accuweather.com//forecasts/v1/`;
 
 
 export default axios.create({baseURL});
