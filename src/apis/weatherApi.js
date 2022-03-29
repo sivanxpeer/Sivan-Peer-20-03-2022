928a0bc35eec6e4de0fb1291fbb34f0059c42ac6
sivanxpeer <sivan.x.peer@gmail.com>
2022-03-29 13:33:49 +0300

    deploy fix

--- src/apis/weatherApi.js
+++ src/apis/weatherApi.js
@@ -1,10 +1,7 @@
 import axios from 'axios';
 
 // const city = "tel%20aviv";
-var cors_api_host = 'cors-anywhere.herokuapp.com';
-var cors_api_url = 'https://' + cors_api_host + '/';
 const baseURL = `https://dataservice.accuweather.com/`;
-// const baseURL = cors_api_url+`//dataservice.accuweather.com/`;
 
 
 
